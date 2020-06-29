22ea90a9df57cef050614a53737761da3226284f
Frank <franklinewax@gmail.com>
2020-06-30 02:57:41 +0300

    implementing when Selection menu is 1 ie. Encryption

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,13 +1,21 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Scanner;
+import java .io.IOException;
+
 public class App {
     public static void main(String[] args ){
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        Scanner value = new Scanner(System.in);
         boolean header = true;
         System.out.println("Welcome to the CAESAR CIPHER Application Console");
         while(header){
             System.out.println("_________*************************************_____________________");
             System.out.println("Choose what you want to do\n" + "1: Encrypting\n "+ "2:Decrypting\n"+ "3: Exit");
+            int selection = value.nextInt();
+            value.nextLine();
+            if(selection ==1){
+
+            }
         }
     }
 }
