50a50e65d2ae7f91d7e4970aee577465a897e1dd
Frank <franklinewax@gmail.com>
2020-06-30 02:47:12 +0300

    Implementing the user logic

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,10 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 public class App {
     public static void main(String[] args ){
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        boolean header = true;
+        System.out.println("Welcome to the CAESAR CIPHER Application Console");
 
     }
 }
