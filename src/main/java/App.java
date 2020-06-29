7442dae1a359a67e80c40f3a318113caf88a015c
Frank <franklinewax@gmail.com>
2020-06-30 02:51:59 +0300

    Creating Menu Item

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,6 +5,9 @@ public class App {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         boolean header = true;
         System.out.println("Welcome to the CAESAR CIPHER Application Console");
-
+        while(header){
+            System.out.println("_________*************************************_____________________");
+            System.out.println("Choose what you want to do\n" + "1: Encrypting\n "+ "2:Decrypting\n"+ "3: Exit");
+        }
     }
 }
