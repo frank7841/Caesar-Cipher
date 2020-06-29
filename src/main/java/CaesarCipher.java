306b72f67de07d2ebd71051ff1a26644e9ad7c53
Frank <franklinewax@gmail.com>
2020-06-29 22:12:56 +0300

    Creating constructor

--- src/main/java/CaesarCipher.java
+++ src/main/java/CaesarCipher.java
@@ -1,10 +1,13 @@
 
 public class CaesarCipher {
     private String inputString;
-    private String encstring;
-    private String decstring
+    private String encString;
+    private String decString;
 
     public CaesarCipher(String inputString) {
-
+        this.inputString = inputString;
+        this.encString = "";
+        this.decString = "";
     }
 }
+
