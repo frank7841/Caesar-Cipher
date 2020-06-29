1ee301385fd19022f7f63d8439a922ae79e8c54f
Frank <franklinewax@gmail.com>
2020-06-29 22:29:29 +0300

    Testing getInputString method

--- src/test/java/CaesarCipherTest.java
+++ src/test/java/CaesarCipherTest.java
@@ -10,6 +10,12 @@ public class CaesarCipherTest {
         assertTrue(cipher instanceof CaesarCipher);
 
     }
+    @Test
+    public void getInputString_returnString_String(){
+        CaesarCipher cipher = new CaesarCipher("testing Input String")
+        assertEquals("testing Input String", cipher.getInputString())
+        }
+
 
 
 }
