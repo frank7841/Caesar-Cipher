fbbf80f65d635a720dce694031dd403d4d695491
Frank <franklinewax@gmail.com>
2020-06-30 00:11:31 +0300

    Implementing Shift right

--- src/main/java/CaesarCipher.java
+++ src/main/java/CaesarCipher.java
@@ -3,6 +3,7 @@ public class CaesarCipher {
     private String inputString;
     private String encString;
     private String decString;
+    private final String[] alphabets = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
 
     public CaesarCipher(String inputString) {
         this.inputString = inputString;
@@ -22,6 +23,26 @@ public class CaesarCipher {
             else if(letter.matches("[^a-zA-Z]")) {
                 encString+=letter;
 
+            }
+            else {
+                if (shiftDirection.equalsIgnoreCase("right")){
+                    for(int x =0;x<alphabets.length;x++){
+                        if(letter.equalsIgnoreCase(alphabets[x])){
+                            if((x+shiftBy)>=alphabets.length){
+                                encString += alphabets[x-26 + shiftBy];
+                            }
+                            else{
+                                encString+=alphabets[x+shiftBy];
+                            }
+                        }
+                    }
+                }
+                else{
+                    for (int x = alphabets.length-1;x>=0;x--){
+                        if
+
+                    }
+                }
             }
         }
 
