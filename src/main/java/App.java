344959fb7c1775285edf51586c4bd458ba78e2a5
Frank <franklinewax@gmail.com>
2020-07-01 14:43:14 +0300

    Lets do this

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,7 +9,7 @@ public class App {
         boolean header = true;
         System.out.println("Welcome to the CAESAR CIPHER Application Console");
         while(header){
-            System.out.println("_________*************************************_____________________");
+            System.out.println("_________*********************Lets Do this****************_____________________");
             System.out.println("Choose what you want to do\n" + "1: Encrypting\n "+ "2:Decrypting\n"+ "3: Exit");
             int selection = value.nextInt();
             value.nextLine();
